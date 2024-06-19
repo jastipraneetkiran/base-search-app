9dba34c162389a1e1175b68b2ceee5d8b012af40
praneet <jastipraneet@gmail.com>
2024-06-19 22:30:52 +0530

    Search Component First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,41 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
+import { environment } from '../environments/environment';
+
+import axios from 'axios';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [FormsModule,NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'base-search-app';
+  // title = 'base-search-app';
+  title = '';
+  error = '';
+  apiKey = environment.apiKey;
+
+  async search() {
+    if (this.title.trim() === '') {
+      this.error = 'Title is required';
+      return;
+    }
+
+    try {
+      const response = await axios.get(`http://www.omdbapi.com/?t=${this.title}&apikey=${this.apiKey}`);
+      if (response.data.Response === 'False') {
+        this.error = response.data.Error;
+      } else {
+        this.error = '';
+        const iframe = document.getElementById('childFrame') as HTMLIFrameElement;
+        iframe?.contentWindow?.postMessage(response.data, '*');
+      }
+    } catch (error) {
+      this.error = 'An error occurred while fetching data';
+    }
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  apiKey: '3804e931'
+};
