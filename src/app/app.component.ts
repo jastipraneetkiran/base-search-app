61c2bbf020a3d56a797fb1d845899231fb8a5898
praneet <jastipraneetkiran@gmail.com>
2024-06-20 11:45:43 +0530

    Added font awsome cdn for loader button in css, added enter keyup property and loader/spinner added to showcase a process is excuting on search button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,24 +18,32 @@ export class AppComponent {
   title = '';
   error = '';
   apiKey = environment.apiKey;
+  loader = false;
 
   async search() {
     if (this.title.trim() === '') {
       this.error = 'Movie/Series Title is required';
       return;
     }
-
+    this.loader = true;
     try {
       const response = await axios.get(`https://www.omdbapi.com/?t=${this.title}&apikey=${this.apiKey}`);
       if (response.data.Response === 'False') {
+        this.loader = false;
         this.error = response.data.Error;
       } else {
         this.error = '';
         const iframe = document.getElementById('childFrame') as HTMLIFrameElement;
         iframe?.contentWindow?.postMessage(response.data, '*');
+        this.loader = false;
       }
     } catch (error) {
       this.error = 'An error occurred while fetching data';
     }
   }
+
+  onEnter() {
+      this.search()
+  }
+
 }
